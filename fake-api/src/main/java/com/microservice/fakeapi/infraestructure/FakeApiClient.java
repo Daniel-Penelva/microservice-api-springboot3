1ad1747292b7d8ad7cec68ad8f6b2048ab80710f
Daniel Penelva <d4n.andrade@gmail.com>
2023-12-28 23:59:13 -0300

    Criando a interface FakeApiClient que representa um cliente feign

---
+++ fake-api/src/main/java/com/microservice/fakeapi/infraestructure/FakeApiClient.java
@@ -0,0 +1,14 @@
+package com.microservice.fakeapi.infraestructure;
+
+import com.microservice.fakeapi.apiv1.dto.ProductsDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@FeignClient(value = "fake-api", url = "${fake-api.url:#{null}}")
+public interface FakeApiClient {
+
+    @GetMapping("/products")
+    List<ProductsDTO> buscaListaProdutos();
+}
